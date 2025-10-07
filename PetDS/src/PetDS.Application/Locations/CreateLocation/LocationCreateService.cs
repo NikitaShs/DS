68524b81c0a380b3edad2cbfc0898af3fcecbf62
Никита <nisc2000123@gmail.com>
2025-10-07 14:15:03 +0300

    Log

--- PetDS/src/PetDS.Application/Locations/CreateLocation/LocationCreateService.cs
+++ PetDS/src/PetDS.Application/Locations/CreateLocation/LocationCreateService.cs
@@ -1,21 +1,23 @@
 ﻿using CSharpFunctionalExtensions;
+using Microsoft.Extensions.Logging;
 using PetDS.Application.abcstractions;
 using PetDS.Domain.Location;
 using PetDS.Domain.Location.VO;
 using PetDS.Domain.Shered;
-using System;
-using System.IO;
-
 
 namespace PetDS.Application.Locations.CreateLocation
 {
     public class LocationCreateService : IHandler<Guid, CreateLocationCommand>
     {
         private readonly ILocationRepository _locationRepository;
+        private readonly ILogger<LocationCreateService> _logger;
 
-        public LocationCreateService(ILocationRepository locationRepository)
+        public LocationCreateService(
+            ILocationRepository locationRepository,
+            ILogger<LocationCreateService> logger)
         {
             _locationRepository = locationRepository;
+            _logger = logger;
         }
 
         public async Task<Result<Guid, Error>> Handel(
@@ -28,12 +30,14 @@ namespace PetDS.Application.Locations.CreateLocation
             {
                 return GeneralErrors.ValueFailure("name");
             }
+
             var loc = Location.Create(name.Value, dto.city, dto.region, dto.strit, dto.namberHouse);
             if (loc.IsFailure)
             {
                 return GeneralErrors.ValueFailure("location");
             }
 
+            _logger.LogInformation("Location создана");
             await _locationRepository.AddLocation(loc.Value, cancellationToken);
 
             return loc.Value.Id.ValueId;
---
+++ PetDS/src/PetDS.Web/Middlewares/ExceptionMiddleware.cs
@@ -0,0 +1,41 @@
+﻿using PetDS.Domain.Shered;
+using PetDS.Web.Middlewares;
+using PetDS.Web.Response;
+using System.Net;
+
+namespace PetDS.Web.Middlewares
+{
+    public class ExceptionMiddleware
+    {
+        public readonly RequestDelegate _next;
+        private readonly ILogger<ExceptionMiddleware> _logger;
+
+        public ExceptionMiddleware(RequestDelegate next, ILogger<ExceptionMiddleware> logger)
+        {
+            _next = next;
+            _logger = logger;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception ex)
+            {
+                var error = Error.Conflict("Exception.server", ex.Message);
+                context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                _logger.LogError(ex, "неизвестная ошибка из Middleware");
+                await context.Response.WriteAsJsonAsync(Envelope.Error(error));
+            }
+        }
+    }
+}
+public static class ExceptionMiddlewareExtensions
+{
+    public static IApplicationBuilder UseExceptionMiddleware(this IApplicationBuilder builder)
+    {
+        return builder.UseMiddleware<ExceptionMiddleware>();
+    }
+}
