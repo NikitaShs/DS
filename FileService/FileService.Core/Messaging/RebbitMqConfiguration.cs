8894bae997763dfdb433261db260b8e22843ee70
Никита <nisc2000123@gmail.com>
2026-02-25 23:05:16 +0300

    просто чтобы сохранить

---
+++ FileService/FileService.Core/Messaging/RebbitMqConfiguration.cs
@@ -0,0 +1,44 @@
+﻿using FileService.Domain.Entites;
+using ImTools;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Wolverine;
+using Wolverine.RabbitMQ;
+
+namespace FileService.Core.Messaging
+{
+    public static class RebbitMqConfiguration
+    {
+        public static void AddRebbitMQ(this WolverineOptions opts, string connectionString)
+        {
+            opts.UseRabbitMq(new Uri(connectionString))
+                .AutoProvision()
+                .EnableWolverineControlQueues()
+                .UseQuorumQueues()
+                .DeclareExchange("exchange", exchange =>
+                {
+                    exchange.ExchangeType = ExchangeType.Topic;
+
+                    exchange.IsDurable = true;
+                });
+
+
+            opts.НастройкаПубликацииРоутинг();
+
+            opts.PublishMessagesToRabbitMqExchange<VideoAsset>(
+                "exchange",
+                m => $"{VideoAsset.BUCKET}").UseDurableOutbox();
+        }
+
+        private static void НастройкаПубликацииРоутинг(this WolverineOptions opts)
+        {
+            opts.PublishMessagesToRabbitMqExchange<VideoAsset>(
+                "exchange",
+                m => $"{VideoAsset.BUCKET}").UseDurableOutbox();
+        }
+
+    }
+}
---
+++ FileService/FileService.Core/Messaging/WolverineConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Builder;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileService.Core.Messaging
+{
+    public static class WolverineConfiguration
+    {
+        public static void AddWolverine(this WebApplicationBuilder builder)
+        {
+
+        }
+    }
+}
